b6e1a24906d440c0d23da9eecdbf45df63e8714c
Manolo Santos <manolo.santos@gmail.com>
2023-06-28 10:44:40 +0200

    fix: Use raw strings instead of escaping

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -3,45 +3,43 @@ import streamlit as st
 from st_chat_message import message
 
 msg1 = """
-Hi! I can pass the butter.
-
-I can also display some **markdown**, including code:
+Hi!, I pass butter and display **markdown**.
 
 ```python
 import streamlit as st
 from st_chat_message import message
 
-message("My message")
-message("Hello bot!", is_user=True)  # align's the message to the right
+message("Hi, there!")
+message("Hello *bot*!", is_user=True)  # aligns the message to the right 
 ```
 """
 
-msg2 = """
-I also can display $\\LaTeX$ formulas:
+msg2 = r"""
+I also can display $\LaTeX$: 
 
 $$
-\\begin{aligned}
-\\frac{d}{dx} \\left( \\int_{a}^{x} f(u)\,du\\right) &= f(x) \\
-\\frac{d}{dx} \\left( \\int_{x}^{b} f(u)\,du\\right) &= -f(x) \\
-\\end{aligned}
+\begin{aligned}
+\frac{d}{dx} \left( \int_{a}^{x} f(u)\,du\right) &= f(x) \
+\frac{d}{dx} \left( \int_{x}^{b} f(u)\,du\right) &= -f(x) \
+\end{aligned}
 $$
 
-I can also display tabular data.
+and tabular data.
 
 ### Maxwell's equations
 
 | Equation | Name |
 | --- | --- |
-| $\\nabla \\cdot \\mathbf{E} = \\frac{\\rho}{\\varepsilon_0}$ | Gauss's law |
-| $\\nabla \\cdot \\mathbf{B} = 0$ | Gauss's law for magnetism |
-| $\\nabla \\times \\mathbf{E} = -\\frac{\\partial \\mathbf{B}}{\\partial t}$ | Faraday's law of induction |
-| $\\nabla \\times \\mathbf{B} = \\mu_0 \\mathbf{J} + \\mu_0 \\varepsilon_0 \\frac{\\partial \\mathbf{E}}{\\partial t}$ | Ampère's circuital law |
+| $\nabla \cdot \mathbf{E} = \frac{\rho}{\varepsilon_0}$ | Gauss's law |
+| $\nabla \cdot \mathbf{B} = 0$ | Gauss's law for magnetism |
+| $\nabla \times \mathbf{E} = -\frac{\partial \mathbf{B}}{\partial t}$ | Faraday's law of induction |
+| $\nabla \times \mathbf{B} = \mu_0 \mathbf{J} + \mu_0 \varepsilon_0 \frac{\partial \mathbf{E}}{\partial t}$ | Ampère's circuital law |
 
 """
 
 if "msgs" not in st.session_state:
     st.session_state.msgs = [{
-        "content": "Hi, what are your capabilites?",
+        "content": "Hi, what is your purpose?",
         "is_user": True,
     }, {
         "content": msg1,
--- examples/streaming.py
+++ examples/streaming.py
@@ -4,26 +4,24 @@ import streamlit as st
 
 from st_chat_message import message
 
-msg1 = """
-Hi! I can pass the butter.
-
-I can also display some **markdown**, including code:
+msg1 = r"""
+Hi! I pass butter and display some **markdown**, including code:
 
 ```python
 import streamlit as st
 from st_chat_message import message
 
 message("My message")
-message("Hello bot!", is_user=True)  # align's the message to the right
+message("Hello bot!", is_user=True)  # aligns the message to the right 
 ```
 
-I also can display $\\LaTeX$ formulas:
+I also can display $\LaTeX$ formulas:
 
 $$
-\\begin{aligned}
-\\frac{d}{dx} \\left( \\int_{a}^{x} f(u)\,du\\right) &= f(x) \\
-\\frac{d}{dx} \\left( \\int_{x}^{b} f(u)\,du\\right) &= -f(x) \\
-\\end{aligned}
+\begin{aligned}
+\frac{d}{dx} \left( \int_{a}^{x} f(u)\,du\right) &= f(x) \
+\frac{d}{dx} \left( \int_{x}^{b} f(u)\,du\right) &= -f(x) \
+\end{aligned}
 $$
 
 I can also display tabular data.
@@ -32,10 +30,10 @@ I can also display tabular data.
 
 | Equation | Name |
 | --- | --- |
-| $\\nabla \\cdot \\mathbf{E} = \\frac{\\rho}{\\varepsilon_0}$ | Gauss's law |
-| $\\nabla \\cdot \\mathbf{B} = 0$ | Gauss's law for magnetism |
-| $\\nabla \\times \\mathbf{E} = -\\frac{\\partial \\mathbf{B}}{\\partial t}$ | Faraday's law of induction |
-| $\\nabla \\times \\mathbf{B} = \\mu_0 \\mathbf{J} + \\mu_0 \\varepsilon_0 \\frac{\\partial \\mathbf{E}}{\\partial t}$ | Ampère's circuital law |
+| $\nabla \cdot \mathbf{E} = \frac{\rho}{\varepsilon_0}$ | Gauss's law |
+| $\nabla \cdot \mathbf{B} = 0$ | Gauss's law for magnetism |
+| $\nabla \times \mathbf{E} = -\frac{\partial \mathbf{B}}{\partial t}$ | Faraday's law of induction |
+| $\nabla \times \mathbf{B} = \mu_0 \mathbf{J} + \mu_0 \varepsilon_0 \frac{\partial \mathbf{E}}{\partial t}$ | Ampère's circuital law |
 
 """
 
